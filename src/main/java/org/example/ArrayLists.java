0dcc29576328ff7b26a0c7c56e2382f797f36c18
Abhishek Mishra <abhishekmishra0513@gmail.com>
2025-03-28 17:51:56 +0530

    Commit 02

--- src/main/java/org/example/ArrayLists.java
+++ src/main/java/org/example/ArrayLists.java
@@ -1,7 +1,22 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+//import java.util.concurrent.ConcurrentMap;
+
+class StringLengthComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        int x = o1.length() - o2.length();
+        if(x == 0)
+            return o1.compareTo(o2);
+        return x;
+    }
+}
+
 
 public class ArrayLists {
     public static void main(String[] args) {
@@ -20,17 +35,30 @@ public class ArrayLists {
 //        catch (Exception e){
 //            System.out.println("Out of bound");
 //        }
-        int k = 1;
-        for(int i =0; i < 10; i++){
-            arrayList.add(k);
-            k++;
-        }
-        for(Integer i : arrayList){
-            System.out.println(i.toString());
-        }
-//        boolean  ans = arrayList.contains(5);
-//        System.out.println(ans);
-        arrayList.remove(2);
-        System.out.println(arrayList);
+//        int k = 1;
+//        for(int i =0; i < 10; i++){
+//            arrayList.add(k);
+//            k++;
+//        }
+//        for(Integer i : arrayList){
+//            System.out.println(i.toString());
+//        }
+////        boolean  ans = arrayList.contains(5);
+////        System.out.println(ans);
+//        arrayList.remove(2);
+//        System.out.println(arrayList);
+
+//        List<String>lst = Arrays.asList("India", "USA", "UK");
+//        List<Integer>lst1 = List.of(1,5,7,4);
+//        lst1.set(1, 5);
+//        List<Integer>lst2 = new ArrayList<>(lst1);
+//        lst2.addAll(lst1);
+//        lst2.remove(Integer.valueOf(2));
+//        lst2.sort(null);
+
+//        System.out.println(lst2);
+        List<String>lst = Arrays.asList("Mango", "Apple", "Banana");
+        lst.sort(new StringLengthComparator());
+        System.out.println(lst);
     }
 }
